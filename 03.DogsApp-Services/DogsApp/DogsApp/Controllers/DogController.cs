2f05c982dade3380ee811a724f66a9e0b9ce67bd
izabellaT <izabellatodorova84@gmail.com>
2023-11-23 10:20:29 +0200

    services dog

---
+++ 03.DogsApp-Services/DogsApp/DogsApp/Controllers/DogController.cs
@@ -0,0 +1,167 @@
+﻿
+using DogsApp.Core.Contracts;
+using DogsApp.Infrastructure.Data;
+using DogsApp.Infrastructure.Data.Domain;
+using DogsApp.Models.Breed;
+using DogsApp.Models.Dog;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+
+namespace DogsApp.Controllers
+{
+    public class DogController : Controller
+    {
+        private readonly IDogService _dogService;
+        private readonly IBreedService _breedService;
+
+        public DogController(IDogService dogsService, IBreedService breedService)
+        {
+            this._dogService = dogsService;
+            _breedService = breedService;
+        }
+
+        public IActionResult Create()
+        {
+            var dog = new DogCreateViewModel();
+            dog.Breeds = _breedService.GetBreeds().Select(c => new BreedPairViewModel()
+            {
+                Id = c.Id,
+                Name = c.Name
+            }).ToList();
+            return View(dog);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm] DogCreateViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+              var createdId =  _dogService.Create(bindingModel.Name, bindingModel.Age, bindingModel.BreedId, bindingModel.Picture);
+                if (createdId)
+                {
+                    return this.RedirectToAction(nameof(Index));
+                }
+            }
+          
+            return this.View();
+        }
+        public IActionResult Edit(int id)
+        {
+            
+            Dog item = _dogService.GetDogById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            DogEditViewModel dog = new DogEditViewModel()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                BreedId = item.BreedId,
+                Age = item.Age,
+                Picture=item.Picture
+            };
+
+            dog.Breeds = _breedService.GetBreeds().Select(c => new BreedPairViewModel()
+            {
+                Id = item.Id,
+                Name = item.Name
+            }).ToList();
+
+            return View(dog);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, DogEditViewModel bindingModel)
+        {
+            if (ModelState.IsValid)
+            {
+                var updated = _dogService.UpdateDog(id, bindingModel.Name, bindingModel.Age, bindingModel.BreedId, bindingModel.Picture);
+                if (updated)
+                {
+                    return this.RedirectToAction("Index");
+                }
+            }
+            return View(bindingModel);
+
+        }
+        
+
+        public IActionResult Delete(int id)
+        {
+            Dog item = _dogService.GetDogById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            DogDetailsViewModel dog = new DogDetailsViewModel()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Age = item.Age,
+                BreedName = item.Breed.Name,
+                Picture = item.Picture
+            };
+            return View(dog);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            var deleted = _dogService.RemoveById(id);
+
+            if (deleted)
+            {
+                return this.RedirectToAction("Index", "Dogs");
+            }
+            else
+            {
+                return View();
+            }
+           
+        }
+            public IActionResult Success()
+        {
+            return this.View();
+
+        }
+
+        public IActionResult Index(string searchStringBreed, string searchStringName)
+        {
+
+            List<DogAllViewModel> dogs = _dogService.GetDogs( searchStringBreed,  searchStringName)
+                .Select(dogFromDb=> new DogAllViewModel
+                {
+                    Id=dogFromDb.Id,
+                    Name = dogFromDb.Name,
+                    Age= dogFromDb.Age,
+                    BreedName=dogFromDb.Breed.Name,
+                    Picture=dogFromDb.Picture
+                }).ToList();
+           
+            return this.View(dogs);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Dog item = _dogService.GetDogById(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            DogDetailsViewModel dog = new DogDetailsViewModel()
+            {
+                Id = item.Id,
+                Name = item.Name,
+                Age = item.Age,
+                BreedName = item.Breed.Name,
+                Picture = item.Picture
+            };
+            return View(dog);
+        }
+        
+        
+    }
+}
